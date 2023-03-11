f278f36d06d1d4361c3ce35a23bdbba00d3d9abc
Dima Zimoykin <zimoykin@gmail.com>
2023-03-11 12:01:49 +0100

    add toxic colors

--- src/components/shared/Header.component.vue
+++ src/components/shared/Header.component.vue
@@ -1,9 +1,9 @@
 <template>
   <div :style="{ height: '10vh' }" class="relative flex justify-center items-center mx-1">
     <div :style="store.isMobileView ? { width: '100%', } : { maxWidth: '1080px', width: '75%', }"
-      class="flex rounded-md justify-center items-center bg-slate-200">
+      class="flex rounded-md justify-center items-center bg-white shadow-xl">
       <p @keydown.enter="goToHome()" @click="goToHome()" class="cursor-pointer text-3xl  mx-1 p-1 rounded-md">🏠</p>
-      <input class="w-full my-2 p-1 bg-slate-200 text-center rounded-md" placeholder="type here..." v-model="searchInput">
+      <input class="w-full my-2 p-1 bg-white text-center rounded-md" placeholder="type here..." v-model="searchInput">
     </div>
   </div>
 </template>
